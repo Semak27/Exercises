c8e0e1863f39846a09d3638f0fe52b1bb8d3308b
Sebastian <sebastianklein279@gmail.com>
2023-09-05 16:01:26 +0200

    Read maze file and display it on the console

---
+++ MazeSolvingAlgorithm/MazeDisplay.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeSolvingAlgorithm
+{
+    public class MazeDisplay
+    {
+        string filePath;
+        string content;
+        int numRows = 0;
+        int numCols = 0;
+        StreamReader sr;
+        char[,] labyrinthMatrix;
+        public MazeDisplay(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public char[,]? ReadData()
+        {
+
+            try
+            {
+                //Ausgabe der txt-Datei in der Konsole
+                content = File.ReadAllText(filePath);
+                content = content.Replace("#", "■");
+                Console.WriteLine(content);
+
+
+                // Zuerst die Anzahl der Zeilen und Spalten im Labyrinth ermitteln
+                numRows = File.ReadLines(filePath).Count();
+                numCols = File.ReadAllLines(filePath)[0].Length;
+
+                labyrinthMatrix = new char[numRows, numCols];
+
+                using (sr = new StreamReader(filePath))
+                {
+                    for (int row = 0; row < numRows; row++)
+                    {
+                        string line = sr.ReadLine();
+
+                        for (int col = 0; col < numCols; col++)
+                        {
+                            // Fülle die Matrix mit den Zeichen aus der Zeile der Textdatei
+                            labyrinthMatrix[row, col] = line[col];
+                        }
+                    }
+                }
+
+                return labyrinthMatrix;
+
+                // Jetzt haben Sie das Labyrinth in einer 2D-Matrix gespeichert
+                // Sie können darauf zugreifen, indem Sie labyrinthMatrix[row, col] verwenden
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Fehler beim Lesen der Datei: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ MazeSolvingAlgorithm/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using MazeSolvingAlgorithm;
+
+MazeDisplay mazeDisplay = new MazeDisplay(@"C:\Users\sebas\source\repos\Exercises\ohne Lösung\labyrinth_10_13.txt");
+mazeDisplay.ReadData();
